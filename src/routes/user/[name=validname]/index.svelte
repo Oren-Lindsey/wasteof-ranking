acaf036f63fa5030ba2df41e19290639474113ba
Lanksy <104015308+lankybox02@users.noreply.github.com>
2022-06-12 17:02:10 +0300

    whoops

--- src/routes/user/[name=validname]/index.svelte
+++ src/routes/user/[name=validname]/index.svelte
@@ -19,15 +19,15 @@
         <div>
             <h1 class="text-3xl">The user <a class="underline hover:text-indigo-500" href="{`https://beta.wasteof.money/@${name}`}">@{name}</a> is: {result}</h1>
             <p>{name} has {followers} followers</p>
-            <p>Their followers have an abnormally small follower count.</p>
-        </div>
+            <p>Their followers have an average follower count of {avg}, a difference of {#if avg > followers}-{diff}{:else}{diff}{/if}</p>
+         </div>
     </div>
     {:else if result == 'botted?'}
     <div class="w-screen h-screen grid place-items-center text-white bg-gray-600 p-4">
         <div>
             <h1 class="text-3xl">The user <a class="underline hover:text-indigo-500" href="{`https://beta.wasteof.money/@${name}`}">@{name}</a> is: {result}</h1>
             <p>{name} has {followers} followers</p>
-            <p>Their followers have an average follower count of {avg}, a difference of {#if avg > followers}-{diff}{:else}{diff}{/if}</p>
+            <p>Their followers have an abnormally small follower count.</p>
         </div>
     </div>
     {:else}
