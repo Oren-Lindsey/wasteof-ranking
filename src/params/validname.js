48b9091733d10205126059e4a031862002495d4a
Oren Lindsey <o.lindsey@icloud.com>
2022-06-12 08:51:19 -0400

    now you can see users with dots in their names

--- src/params/validname.js
+++ src/params/validname.js
@@ -1,4 +1,8 @@
 /** @type {import('@sveltejs/kit').ParamMatcher} */
 export function match(param) {
+  if (param !== 'willy.' && param !== 'wasteof.money') {
     return /^[a-z0-9_\\-]{1,20}$/.test(param);
+  } else {
+    return true
   }
+}
